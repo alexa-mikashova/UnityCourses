af62529cc5b1556574e135ee076e15cf5aac4c15
Alexandra Mikashova <alexa.mikashova@gmail.com>
2024-05-04 17:14:53 +0300

    Add new Logic PingPong

--- Homework1/Assets/Scripts/PingPong.cs
+++ Homework1/Assets/Scripts/PingPong.cs
@@ -1,36 +1,29 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class PingPong : MonoBehaviour
 {
-    public GameObject sphere;
-    public float speed = 6.0f;
-    private Vector3 positionSphere;
-    private Vector3 positionPoint;
-    private bool flag = false;
-    // Start is called before the first frame update
+    public float TargetTime;
+    private float currentTime;
+    private Vector3 pointStart;
+    private Vector3 pointEnd;
     void Start()
     {
-        positionSphere = sphere.transform.position; // начальна€ точка
-        positionPoint = new Vector3(Random.Range(positionSphere.x, positionSphere.x + 10.0f), 0, 0); // конечна€ точка
+        pointStart = transform.position;
+        // √енераци€ случайной конечной точки в трехмерном пространстве
+        pointEnd = new Vector3(
+            Random.Range(transform.position.x, transform.position.x + 10.0f),
+            Random.Range(transform.position.y, transform.position.y + 10.0f),
+            Random.Range(transform.position.z, transform.position.z + 10.0f)
+        );
     }
 
-    // Update is called once per frame
     void Update()
     {
-        if (sphere.transform.position.x < positionPoint.x && !flag)
-        {
-            sphere.transform.Translate(speed * Time.deltaTime, 0, 0);
-        }
-        else if (sphere.transform.position.x > positionSphere.x)
-        {
-            flag = true;
-            sphere.transform.Translate(-speed * Time.deltaTime, 0, 0);
-        }
-        else
-        {
-            flag = false;
-        }
+        currentTime += Time.deltaTime;
+        var normTime = currentTime / TargetTime;
+        transform.position = Vector3.Lerp(pointStart, pointEnd, Mathf.PingPong(normTime, 1.0f));
     }
 }
